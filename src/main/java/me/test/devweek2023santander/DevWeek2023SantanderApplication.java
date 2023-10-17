88cd2dee56dc0545629588778ad884ad14ee79bc
Djair Alves <djair.dj@gmail.com>
2023-10-17 12:19:36 -0300

    Códigos iniciais

---
+++ src/main/java/me/test/devweek2023santander/DevWeek2023SantanderApplication.java
@@ -0,0 +1,13 @@
+package me.test.devweek2023santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeek2023SantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeek2023SantanderApplication.class, args);
+	}
+
+}
